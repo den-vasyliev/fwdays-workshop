c3e241032884552b2633c1840436c54932e9cdb8
Denys Vasyliev <den.vasyliev@gmail.com>
2024-06-27 00:07:40 +0100

    Update main.tf

--- tf-gke-cluster/main.tf
+++ tf-gke-cluster/main.tf
@@ -8,41 +8,46 @@ terraform {
 }
 
 variable "project_id" {
-  description = "The project ID to deploy resources to" 
+  description = "The project ID to deploy resources to"
 }
 
 variable "location" {
   description = "The location to deploy resources to"
   default     = "us-central1-a"
-  
+
 }
 
 variable "region" {
   description = "The region to deploy resources to"
   default     = "us-central1"
-} 
+}
 
 variable "cluster_name" {
   description = "The name of the GKE cluster"
   default     = "my-gke-cluster"
-  
+
+}
+
+variable "machine_type" {
+  description = "The machine type to use for the default node pool"
+  default     = "e2-medium"
 }
 
 variable "credentials" {
   description = "The path to the service account key file"
-  sensitive = true
+  sensitive   = true
 }
 
 provider "google" {
-  project = "${var.project_id}"
-  region  = "${var.region}"
-  zone    = "${var.location}"
-  credentials = "${var.credentials}"
+  project     = var.project_id
+  region      = var.region
+  zone        = var.location
+  credentials = var.credentials
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.cluster_name}"
-  location = "${var.location}"
+  name     = var.cluster_name
+  location = var.location
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -56,49 +61,70 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_container_node_pool" "default_pool" {
-  name       = "default-pool"
-  location   = "us-central1-a"
+resource "google_container_node_pool" "cpu_pool" {
+  name       = "cpu-pool"
+  location   = var.location
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 2
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = var.machine_type
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-}
 
+}
 resource "google_container_node_pool" "gpu_pool" {
-  name       = "gpu-pool"
-  location   = "us-central1-a"
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
+  name     = "gpu-pool"
+  location = "us-central1-a"
+  cluster  = google_container_cluster.primary.name
+
+  initial_node_count = 0
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 1
+  }
 
   node_config {
-    preemptible  = true
-    machine_type = "n1-standard-4"  # Adjust based on your GPU requirements
+    machine_type = "n1-standard-4"
 
     guest_accelerator {
-      type  = "nvidia-tesla-t4"  # Adjust based on your GPU requirements
+      type  = "nvidia-tesla-t4"
       count = 1
     }
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
 
-    # Install GPU drivers
     metadata = {
       install-nvidia-driver = "true"
     }
+
+    labels = {
+      "cloud.google.com/gke-accelerator" = "nvidia-tesla-t4"
+    }
+
+    taint {
+      key    = "nvidia.com/gpu"
+      value  = "present"
+      effect = "NO_SCHEDULE"
+    }
+
+    # Use spot instances
+    spot = true
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 }
 
